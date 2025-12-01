f3c45c9c79307cc27ede4491eeb8055daf27c182
llucasgm <lucas.gomesmm19@gmail.com>
2025-12-01 09:52:58 -0300

    feat: Tentativa login 4

--- src/Main/Models/Usuarios.js
+++ src/Main/Models/Usuarios.js
@@ -5,6 +5,7 @@ class Usuarios {
 
   buscarPorEmail(email) {
     const db = getDb();
+    
     const stmt = db.prepare(`
       SELECT * FROM usuario 
       WHERE email_usuario = ? AND status_usuario = 'ativo' AND excluido_em IS NULL
