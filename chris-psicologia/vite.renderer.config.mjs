b0d079c824c03dae8e04366db6cee818c68c1ccb
JeanTrey <jeanmarconascimentodarocha@gmail.com>
2025-12-03 09:25:03 -0300

    DESKTOP do chris pagamentos2

--- chris-psicologia/vite.renderer.config.mjs
+++ chris-psicologia/vite.renderer.config.mjs
@@ -1,4 +1,15 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config
-export default defineConfig({});
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        // Define os pontos de entrada do seu aplicativo
+        main: resolve(__dirname, 'index.html'),
+        pagamento: resolve(__dirname, 'src/Renderer/Views/Pagamento/pagamento.html'),
+      },
+    },
+  },
+});
